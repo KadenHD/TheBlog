e2788a9c9e9740a42f57efeeaa75e9b9073fd9ac
KadenHD <71968563+KadenHD@users.noreply.github.com>
2021-08-08 19:18:52 +0200

    add new controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,8 +9,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -72,76 +70,5 @@ class HomeController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/profile", name="profile")
-     */
-    public function profile() {
-        $user = $this->getUser();
 
-        if ($user == null) {
-            return $this->redirectToRoute('app_login');
-        }
-
-        return $this->render('user/profile.html.twig', [
-            "user" => $user,
-        ]);
-        
-    }
-
-    /**
-     * @Route("/profile/{id}", name="delete_article", methods={"GET", "DELETE"})
-     */
-    public function deleteArticle(Article $article)
-    {
-        $emi=$this->getDoctrine()->getManager();
-        $emi->remove($article);
-        $emi->flush();
-
-        return $this->redirectToRoute('profile');
-    }
-
-    /**
-     * @Route("/articles", name="create_article")
-     */
-    public function create(Request $request, EntityManagerInterface $emi)
-    {   
-        $article = new Article();
-
-        $form = $this->createFormBuilder($article)
-            ->add("titre", TextType::class)
-            ->add("image", UrlType::class)
-            ->add("contenu", CKEditorType::class, [
-                'required' => true,
-                'constraints'=>[
-                    new NotBlank(),
-                ]
-            ])
-            ->add("submit", SubmitType::class, [
-                "attr" => [
-                    "class" => "btn btn-primary",
-                    "style" => "margin-top: 0.5%;%"
-                ]
-            ])
-
-            ->getForm();
-
-        $user = $this->getUser();
-
-        $article->setAuteur($user);
-        $article->setDate(new \DateTime());
-        
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $emi->persist($article);
-            $emi->flush();
-            return $this->redirectToRoute('profile');
-        }
-
-        return $this->render('user/createArticle.html.twig', [
-            "article" => $article,
-            "form" => $form->createView()
-        ]);
-    }
 }
